457c68ff5451bc195107d69d27c7446d1e6e6e61
zhaomaosheng <zmshahaha@gmail.com>
2024-09-20 01:02:11 +0800

    first impl, speed slow

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock;
 
 double
 now()
@@ -78,7 +79,9 @@ put_thread(void *xa)
   int b = NKEYS/nthread;
 
   for (int i = 0; i < b; i++) {
+    pthread_mutex_lock(&lock);
     put(keys[b*n + i], n);
+    pthread_mutex_unlock(&lock);
   }
 
   return NULL;
@@ -114,6 +117,8 @@ main(int argc, char *argv[])
   tha = malloc(sizeof(pthread_t) * nthread);
   srandom(0);
   assert(NKEYS % nthread == 0);
+  pthread_mutex_init(&lock, NULL);
+
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
